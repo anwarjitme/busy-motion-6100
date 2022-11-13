6d7552022fd1bb88cc5916305762d090a01dbfbc
Md Anwar Hossain201~ <anwarjitme.com>
2022-11-13 10:40:47 +0530

    amm

--- lifee_app/src/Components/Men.jsx
+++ lifee_app/src/Components/Men.jsx
@@ -1,17 +1,61 @@
 import React from 'react'
 import Navbar from '../Navbar'
-import { Box, Text } from "@chakra-ui/react";
-
+import { Box, Image, Text } from "@chakra-ui/react";
+import Carousel from "framer-motion-carousel";
 const Men = () => {
   return (
-    
-      <div width="100%">
-        <Navbar />
-        <Box width="100%" h="30px" bg="yellow">
-          <Text>Lingerie And Nigntwear from 299</Text>
-        </Box>
-      </div>
-    
+    <div width="100%">
+      <Navbar />
+      <Box width="100%" h="40px" bg="yellow">
+        <Text color="white" m="auto" textAlign="center">
+          Lingerie And Nigntwear from 299
+        </Text>
+      </Box>
+      <Carousel>
+        <Image
+          width="550px "
+          height="580px"
+          className="logo"
+          src="https://www.499rupees.com/images/store/img_1526970690.jpg"
+          alt="logo"
+        />
+        <Image
+          width="550px "
+          height="580px"
+          className="logo"
+          src="https://www.499rupees.com/images/store/img_1526970690.jpg"
+          alt="logo"
+        />
+        <Image
+          width="550px "
+          height="580px"
+          className="logo"
+          src="https://www.499rupees.com/images/store/img_1526970690.jpg"
+          alt="logo"
+        />
+        <Image
+          width="550px "
+          height="580px"
+          className="logo"
+          src="https://www.499rupees.com/images/store/img_1526970690.jpg"
+          alt="logo"
+        />
+        <Image
+          width="550px "
+          height="580px"
+          className="logo"
+          src="https://www.499rupees.com/images/store/img_1526970690.jpg"
+          alt="logo"
+        />
+        <Image
+          width="550px "
+          height="580px"
+          className="logo"
+          src="https://www.499rupees.com/images/store/img_1526970690.jpg"
+          alt="logo"
+        />
+      </Carousel>
+    </div>
   );
 }
 
